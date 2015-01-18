9ed922c975567fcdc2163594e62456919e755b57
Kyle Rosales <kyle.rosales97@gmail.com>
2015-01-19 00:38:14 +0800

    customized admin form

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
 from polls.models import Poll
 
+#default
+'''
 admin.site.register(Poll)
+#'''
+
+#customized admin from
+#'''
+class PollAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question']
+
+admin.site.register(Poll, PollAdmin)
+#'''
