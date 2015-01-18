8d41960d1c14bf00f41587bf96e65e6ce7a0315a
Kyle Rosales <kyle.rosales97@gmail.com>
2015-01-19 00:59:15 +0800

    added option to add choices when adding new poll

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 from polls.models import Poll
 from polls.models import Choice
 
-admin.site.register(Choice)
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+#admin.site.register(Choice)
 
 #collapsed fieldset
 #'''
@@ -11,6 +15,7 @@ class PollAdmin(admin.ModelAdmin):
         ('info',               {'fields': ['question']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
+    inlines = [ChoiceInline]
 
 admin.site.register(Poll, PollAdmin)
 #'''
